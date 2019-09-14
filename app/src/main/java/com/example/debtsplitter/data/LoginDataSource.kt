8c2651e565ae2efce2249c92eda3dda6877beb3d
Foel_ <indifikat@gmail.com>
2019-09-14 17:49:47 +0300

    add auth page tel box pass box pre-validation

---
+++ app/src/main/java/com/example/debtsplitter/data/LoginDataSource.kt
@@ -0,0 +1,25 @@
+package com.example.debtsplitter.data
+
+import com.example.debtsplitter.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
+
