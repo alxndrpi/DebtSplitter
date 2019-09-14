22af21bbf3cba2b2dfed877f4cda7ea694d98cfb
a.maskaev <a.maskaev@MAC407RIH.local>
2019-09-14 22:46:17 +0300

    add next screen button

--- app/src/main/java/com/example/debtsplitter/PartyActivity.kt
+++ app/src/main/java/com/example/debtsplitter/PartyActivity.kt
@@ -1,12 +1,20 @@
 package com.example.debtsplitter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_party.*
 
 class PartyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_party)
+
+
+        new_party_button.setOnClickListener {
+            val intent = Intent(this, InputChoiceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
