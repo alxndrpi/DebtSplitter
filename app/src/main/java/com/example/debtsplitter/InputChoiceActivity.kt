dc49cd14878b9febaaada8fc09a5b805f591dce6
a.pismenskiy <a.pismenskiy@rambler-co.ru>
2019-09-15 07:58:03 +0300

    added QR code scanner with FNS interaction

--- app/src/main/java/com/example/debtsplitter/InputChoiceActivity.kt
+++ app/src/main/java/com/example/debtsplitter/InputChoiceActivity.kt
@@ -2,19 +2,13 @@ package com.example.debtsplitter
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.ArrayMap
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
-import kotlinx.android.synthetic.main.activity_input_choice.*
 import kotlinx.android.synthetic.main.content_input_choice.*
 import kotlinx.android.synthetic.main.navigation_layout.*
 
 
 class InputChoiceActivity : AppCompatActivity() {
-
-    lateinit var toolbar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +17,17 @@ class InputChoiceActivity : AppCompatActivity() {
         val bottomNavigation: BottomNavigationView = navigationView
         bottomNavigation.setOnNavigationItemSelectedListener(::navigationListener)
 
-        manualInputButton.setOnClickListener{
+        manualInputButton.setOnClickListener {
             val data = "sada"
             val intent = Intent(this, ReceiptActivity::class.java)
             intent.putExtra("check_data", data)
             startActivity(intent)
         }
 
+        scanQRButton.setOnClickListener {
+            val intent = Intent(this@InputChoiceActivity, ScanActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
